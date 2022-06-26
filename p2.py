dfcaa5176a96547084eb30d994672df97a7d1660
sutherland11 <108205898+sutherland11@users.noreply.github.com>
2022-06-26 15:44:59 -0600

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,9 +1,12 @@
-
+# Finds the sum of the even-valued terms in the fibonacci sequence whose value is less than some max number, 4,000,000 in this case.
 def sumEvenFibonacci(maxNum):
+    # start by setting the total to 0, and the first 2 terms to 1 and 2 respectively.
     total = 0 
     termA = 0
     termB = 1
-    while termB < maxNum:
+    # Loop until the larger term is greater than or equal to the given max number.
+    while termB <= maxNum:
+        # set the first term to the value of the 2nd and the second to the sum of the first and second term then add the 2nd term to the total if it is even.
         termA, termB = termB, termA + termB
         total += termB if termB % 2 == 0 else 0
     return total
