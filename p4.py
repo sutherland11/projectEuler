c04031d24790672053abb3b776b0d51c0236a392
sutherland11 <108205898+sutherland11@users.noreply.github.com>
2022-06-26 16:26:27 -0600

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,18 @@
+# Finds the largest numbe rthat is a product of 2 3-digit number and a palindrome.
+
+# Ths function simply returns true of the 2 factors produce a product that is a plaindrome.
+def isPalindrome(num1, num2):
+    return str(num1 * num2) == str(num1 * num2)[::-1]
+
+# Main method, there is almost definitely a more efficient method but since the computation time on this problem is small I thought this would be sufficient.
+def main():
+    # Set largest palindrome to 0 initially
+    largestPalindrome = 0
+    # nested for loop will iterate through all possible combinations of 3-digit factors to determine the largest palindrome.
+    for factor1 in range(999, 100, -1):
+        for factor2 in range(factor1, 100, -1):
+            if isPalindrome(factor1, factor2) and factor1 * factor2 > largestPalindrome:
+                largestPalindrome = factor1 * factor2
+    return largestPalindrome
+
+print(main())
