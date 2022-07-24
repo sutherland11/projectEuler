038fd9aaf2be86010e0601228a1a7579e5d38979
sutherland11 <108205898+sutherland11@users.noreply.github.com>
2022-07-24 10:27:00 -0600

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,26 @@
+from traceback import format_exc
+from math import prod
+
+# Main method finds the largest product of N numbers in a given series.
+def main():
+    # opening file at given path and creating a list
+    path = 'C:\\Users\\wills\\OneDrive\\Desktop\\myPyScripts\\ProjectEuler\\8\\numFile.txt'
+    file = open(path, 'r')
+    nums = [int(num) for num in file.read() if num != '\n']
+
+    # simply iterating through the list and finding the product of 13 consecutive numbers, setting the product as the largest if it 
+    # is larger than the previous largest.
+    N = 13
+    maxProduct = 0
+    index = 0 
+    while index < len(nums) - N:
+        product = prod(nums[index : index + N])
+        maxProduct = product if product > maxProduct else maxProduct
+        index += 1
+    print(maxProduct)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        print(format_exc())
